a859183e73fdaef7196f2c0ec8f6c3a00e94f96c
LangVE <langve0@gmail.com>
2020-11-24 02:28:25 +0900

    2장 test 코드 06

---
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -0,0 +1,23 @@
+package iloveyouboss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProfileTest {
+    @Test
+    public void test() {
+        // given
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got bonuses?");
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+        criteria.add(criterion);
+
+        // when
+
+        // then
+    }
+
+}
