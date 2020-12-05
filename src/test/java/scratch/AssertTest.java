69879c4be510fcc6b8363b277c7c50dc11f477df
LangVE <langve0@gmail.com>
2020-12-06 02:02:24 +0900

    3장 3.1.3 코드

--- src/test/java/scratch/AssertTest.java
+++ src/test/java/scratch/AssertTest.java
@@ -3,6 +3,8 @@ package scratch;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -92,11 +94,116 @@ public class AssertTest {
 
     @Test
     public void getName_whenMatchersStartWith_thenFail() {
+        try {
+            // given
+
+            // when
+
+            // then
+            assertThat(account.getName(), startsWith("xyz"));
+        } catch (AssertionError e) {
+            System.out.println("의도된 AssertError 발생");
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void stringArray_whenMatchersEqualTo_thenFail() {
+        try {
+            // given
+
+            // when
+
+            // then
+            assertThat(new String[]{"a", "b", "c"}, equalTo(new String[]{"a", "b"}));
+        } catch (AssertionError e) {
+            System.out.println("의도된 AssertError 발생");
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void stringArray_whenMatchersEqualTo_thenPass() {
         // given
 
         // when
 
         // then
-        assertThat(account.getName(), startsWith("xyz"));
+        assertThat(new String[]{"a", "b"}, equalTo(new String[]{"a", "b"}));
+    }
+
+    @Test
+    public void stringList_whenMatchersEqualTo_thenFail() {
+        try {
+            // given
+
+            // when
+
+            // then
+            assertThat(Arrays.asList(new String[]{"a"}), equalTo(Arrays.asList(new String[]{"a", "ab"})));
+        } catch (AssertionError e) {
+            System.out.println("의도된 AssertError 발생");
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void stringList_whenMatchersEqualTo_thenPass() {
+        // given
+
+        // when
+
+        // then
+        assertThat(Arrays.asList(new String[]{"a"}), equalTo(Arrays.asList(new String[]{"a"})));
+    }
+
+    @Test
+    public void getName_whenMatchersEqualTo_thenPass() {
+        // given
+        Account account = new Account("my big fat acct");
+
+        // when
+        String name = account.getName();
+
+        // then
+        assertThat(name, is(equalTo("my big fat acct")));
+    }
+
+    @Test
+    public void getName_whenMatchersNotEqualTo_thenPass() {
+        // given
+        Account account = new Account("my big fat acct");
+
+        // when
+        String name = account.getName();
+
+        // then
+        assertThat(name, not(equalTo("plunderings")));
+    }
+
+    @Test
+    public void getName_whenMatchersNotNullValue_thenPass() {
+        // given
+        Account account = new Account("my big fat acct");
+
+        // when
+        String name = account.getName();
+
+        // then
+        assertThat(name, is(not(nullValue())));
+    }
+
+    @Test
+    public void getName_whenMatchersIsNotNullValue_thenPass() {
+        // given
+        Account account = new Account("my big fat acct");
+
+        // when
+        String name = account.getName();
+
+        // then
+        assertThat(name, is(notNullValue())); // 유용하지 않음
+        assertThat(name, equalTo("my big fat acct"));
     }
 }
