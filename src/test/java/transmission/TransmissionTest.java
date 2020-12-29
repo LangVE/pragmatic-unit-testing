f6fee656d5b82c29ad92a15ecf181ae6f33cbbf8
LangVE <langve0@gmail.com>
2020-12-29 11:35:41 +0900

    7장 7.4 코드

---
+++ src/test/java/transmission/TransmissionTest.java
@@ -0,0 +1,46 @@
+package transmission;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class TransmissionTest {
+   private Transmission transmission;
+   private Car car;
+
+   @Before
+   public void create() {
+      car = new Car();
+      transmission = new Transmission(car);
+   }
+
+   @Test
+   public void remainsInDriveAfterAcceleration() {
+      transmission.shift(Gear.DRIVE);
+      car.accelerateTo(35);
+      assertThat(transmission.getGear(), equalTo(Gear.DRIVE));
+   }
+
+   @Test
+   public void ignoresShiftToParkWhileInDrive() {
+      transmission.shift(Gear.DRIVE);
+      car.accelerateTo(30);
+
+      transmission.shift(Gear.PARK);
+
+      assertThat(transmission.getGear(), equalTo(Gear.DRIVE));
+   }
+
+   @Test
+   public void allowsShiftToParkWhenNotMoving() {
+      transmission.shift(Gear.DRIVE);
+      car.accelerateTo(30);
+      car.brakeToStop();
+
+      transmission.shift(Gear.PARK);
+
+      assertThat(transmission.getGear(), equalTo(Gear.PARK));
+   }
+}
