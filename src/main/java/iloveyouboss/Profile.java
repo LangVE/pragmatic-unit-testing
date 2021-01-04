729c1e6e7c6682d886c4cfd7ed4de4a45f24162d
LangVE <langve0@gmail.com>
2021-01-05 01:58:43 +0900

    8장 8.1 코드

--- src/main/java/iloveyouboss/Profile.java
+++ src/main/java/iloveyouboss/Profile.java
@@ -5,7 +5,7 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/utj2 for more book information.
-***/
+ ***/
 package iloveyouboss;
 
 import java.util.HashMap;
@@ -14,55 +14,58 @@ import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class Profile { 
-   private Map<String,Answer> answers = new HashMap<>();
-   private int score;
-   private String name;
+public class Profile {
+    private Map<String, Answer> answers = new HashMap<>();
+    private int score;
+    private String name;
 
-   public Profile(String name) {
-      this.name = name;
-   }
-   
-   public String getName() {
-      return name;
-   }
+    public Profile(String name) {
+        this.name = name;
+    }
 
-   public void add(Answer answer) { 
-      answers.put(answer.getQuestionText(), answer);
-   }
-   
-   public boolean matches(Criteria criteria) { 
-      score = 0;
-      
-      boolean kill = false;
-      boolean anyMatches = false; 
-      for (Criterion criterion: criteria) {   
-         Answer answer = answers.get(
-               criterion.getAnswer().getQuestionText()); 
-         boolean match = 
-               criterion.getWeight() == Weight.DontCare || 
-               answer.match(criterion.getAnswer());
+    public String getName() {
+        return name;
+    }
 
-         if (!match && criterion.getWeight() == Weight.MustMatch) {  
-            kill = true;
-         }
-         if (match) {         
-            score += criterion.getWeight().getValue();
-         }
-         anyMatches |= match;
-      }
-      if (kill)
-         return false;
-      return anyMatches;
-   }
+    public void add(Answer answer) {
+        answers.put(answer.getQuestionText(), answer);
+    }
 
-   public int score() {
-      return score;
-   }
+    public boolean matches(Criteria criteria) {
+        score = 0;
 
-   public List<Answer> find(Predicate<Answer> pred) {
-      return answers.values().stream()
-              .filter(pred)
-              .collect(Collectors.toList());
-   }
+        boolean kill = false;
+        boolean anyMatches = false;
+        for (Criterion criterion : criteria) {
+            Answer answer = answers.get(
+                    criterion.getAnswer().getQuestionText());
+            boolean match = matches(criterion, answer);
+
+            if (!match && criterion.getWeight() == Weight.MustMatch) {
+                kill = true;
+            }
+            if (match) {
+                score += criterion.getWeight().getValue();
+            }
+            anyMatches |= match;
+        }
+        if (kill)
+            return false;
+        return anyMatches;
+    }
+
+    private boolean matches(Criterion criterion, Answer answer) {
+        return criterion.getWeight() == Weight.DontCare ||
+                answer.match(criterion.getAnswer());
+    }
+
+    public int score() {
+        return score;
+    }
+
+    public List<Answer> find(Predicate<Answer> pred) {
+        return answers.values().stream()
+                .filter(pred)
+                .collect(Collectors.toList());
+    }
 }
