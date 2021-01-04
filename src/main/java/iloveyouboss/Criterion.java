3ca4baf7dcaaea3683e1bc7c2c18b5a097989788
LangVE <langve0@gmail.com>
2021-01-05 02:02:45 +0900

    8장 8.2 코드

--- src/main/java/iloveyouboss/Criterion.java
+++ src/main/java/iloveyouboss/Criterion.java
@@ -13,14 +13,29 @@ public class Criterion implements Scoreable {
    private Answer answer;
    private int score;
 
-   public Criterion(Answer answer, Weight weight) {
-      this.answer = answer;
-      this.weight = weight;
-   }
-   
-   public Answer getAnswer() { return answer; }
-   public Weight getWeight() { return weight; }
-   
-   public void setScore(int score) { this.score = score; }
-   public int getScore() { return score; }
+    public Criterion(Answer answer, Weight weight) {
+        this.answer = answer;
+        this.weight = weight;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public Weight getWeight() {
+        return weight;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    boolean matches(Answer answer) {
+        return getWeight() == Weight.DontCare ||
+                answer.match(getAnswer());
+    }
 }
