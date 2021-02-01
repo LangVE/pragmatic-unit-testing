5a4580576e0aa950b922fafa38956d3e2e4558cd
LangVE <langve0@gmail.com>
2021-02-02 02:47:15 +0900

    11장 11.3 코드

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -7,44 +7,41 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 import java.util.logging.Level;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
+import static util.ContainsMatches.containsMatches;
 
 public class SearchTest {
-        @Test
-        public void testSearch() throws IOException {
-                String pageContent = "There are certain queer times and occasions "
-                        + "in this strange mixed affair we call life when a man "
-                        + "takes this whole universe for a vast practical joke, "
-                        + "though the wit thereof he but dimly discerns, and more "
-                        + "than suspects that the joke is at nobody's expense but "
-                        + "his own.";
-                byte[] bytes = pageContent.getBytes();
-                ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
-                // search
-                Search search = new Search(stream, "practical joke", "1");
-            Search.LOGGER.setLevel(Level.OFF);
-            search.setSurroundingCharacterCount(10);
-            search.execute();
-            assertFalse(search.errored());
-            List<Match> matches = search.getMatches();
-            assertTrue(matches.size() >= 1);
-            Match match = matches.get(0);
-            assertThat(match.searchString, equalTo("practical joke"));
-            assertThat(match.surroundingContext,
-                    equalTo("or a vast practical joke, though t"));
-            stream.close();
+    @Test
+    public void testSearch() throws IOException {
+        String pageContent = "There are certain queer times and occasions "
+                + "in this strange mixed affair we call life when a man "
+                + "takes this whole universe for a vast practical joke, "
+                + "though the wit thereof he but dimly discerns, and more "
+                + "than suspects that the joke is at nobody's expense but "
+                + "his own.";
+        byte[] bytes = pageContent.getBytes();
+        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+        // search
+        Search search = new Search(stream, "practical joke", "1");
+        Search.LOGGER.setLevel(Level.OFF);
+        search.setSurroundingCharacterCount(10);
+        search.execute();
+        assertFalse(search.errored());
+        assertThat(search.getMatches(), containsMatches(new Match[]{
+                new Match("1", "practical joke",
+                        "or a vast practical joke, though t")
+        }));
+        stream.close();
 
-            // negative
-            URLConnection connection =
-                    new URL("http://bit.ly/15sYPA7").openConnection();
-            InputStream inputStream = connection.getInputStream();
-            search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
-            search.execute();
-            assertThat(search.getMatches().size(), equalTo(0));
-            stream.close();
+        // negative
+        URLConnection connection =
+                new URL("http://bit.ly/15sYPA7").openConnection();
+        InputStream inputStream = connection.getInputStream();
+        search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
+        search.execute();
+        assertTrue(search.getMatches().isEmpty());
+        stream.close();
     }
 }
