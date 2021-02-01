f8c55b040e1ac267005d2ac72bee78894c671b38
LangVE <langve0@gmail.com>
2021-02-02 03:29:04 +0900

    11장 11.8 코드

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -36,10 +36,11 @@ public class SearchTest {
                 + "though the wit thereof he but dimly discerns, and more "
                 + "than suspects that the joke is at nobody's expense but "
                 + "his own.");
-
         Search search = new Search(stream, "practical joke", A_TITLE);
         search.setSurroundingCharacterCount(10);
+
         search.execute();
+
         assertFalse(search.errored());
         assertThat(search.getMatches(), containsMatches(new Match[]{
                 new Match(A_TITLE, "practical joke",
@@ -53,7 +54,9 @@ public class SearchTest {
                 new URL("http://bit.ly/15sYPA7").openConnection();
         stream = connection.getInputStream();
         Search search = new Search(stream, "smelt", A_TITLE);
+
         search.execute();
+
         assertTrue(search.getMatches().isEmpty());
     }
 
