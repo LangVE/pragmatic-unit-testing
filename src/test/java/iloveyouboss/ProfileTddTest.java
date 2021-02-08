27fbe014d6e9d6438fe74fd2014212556cb2a5c7
LangVE <langve0@gmail.com>
2021-02-09 00:06:52 +0900

    12장 12.3 코드

--- src/test/java/iloveyouboss/ProfileTddTest.java
+++ src/test/java/iloveyouboss/ProfileTddTest.java
@@ -3,6 +3,7 @@ package iloveyouboss;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProfileTddTest {
     @Test
@@ -14,5 +15,18 @@ public class ProfileTddTest {
         boolean result = profileTdd.matches(criterion);
 
         assertFalse(result);
+    }
+
+    @Test
+    public void matchesWhenProfileContainsMatchingAnswer() {
+        ProfileTdd profileTdd = new ProfileTdd();
+        Question question = new BooleanQuestion(1, "Relocation package?");
+        Answer answer = new Answer(question, Bool.TRUE);
+        profileTdd.add(answer);
+        Criterion criterion = new Criterion(answer, Weight.DontCare);
+
+        boolean result = profileTdd.matches(criterion);
+
+        assertTrue(result);
     }
 }
